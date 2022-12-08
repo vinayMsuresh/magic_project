d8271c0599a33c2e50441c988f9a6e7526e2d3fd
vinayMsuresh <vinay.suresh@neosoftmail.com>
2022-12-08 15:23:37 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Login from './components/Login/Login';
+import { useState } from 'react';
 
 function App() {
+  const [show, setShow] = useState(true)
   return (
     <div className="App">
-      
+      <Login show={show} handleClose={()=>setShow(false)} />
     </div>
   );
 }
