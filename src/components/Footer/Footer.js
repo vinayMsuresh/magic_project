ed94ab9f83fc32c32951cc42c784c587c0e7902c
jayvsajay <jayvsajay734@gmail.com>
2022-12-08 13:20:14 +0530

    changes made

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './footer.css';
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Navbar
