00d4a33304ad15a606e3a3f9e6d9e7d684c6df75
vinayMsuresh <vinay.suresh@neosoftmail.com>
2022-12-09 17:22:27 +0530

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Login from './components/Login/Login';
-import { useState } from 'react';
-import Price from './components/pricing/Price';
-=======
 import './App.scss';
 import NavBar from './components/Navbar/NavBar';
 import Login from './components/Login/Login';
@@ -13,23 +6,16 @@ import { useState } from 'react';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import Dashboard from './components/Dashboard/Dashboard';
->>>>>>> 94bfffffa701cfb3b1c2aea3809ac252df5b3a32
 
 function App() {
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(false);
   return (
     <div>
-<<<<<<< HEAD
-      {/* <Navbar/> */}
-      <Price/>
-      <Login show={show} handleClose={()=>setShow(false)} />
-=======
       <NavBar/>
       <Home/>
       <Dashboard/>
       <Login show={show} handleClose={()=>setShow(false)} />
       <Footer/>
->>>>>>> 94bfffffa701cfb3b1c2aea3809ac252df5b3a32
     </div>
   );
 }
