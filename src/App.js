36f6ff8a595b61a63dfe22cc814ba90034fbfc7b
vinayMsuresh <vinay.suresh@neosoftmail.com>
2022-12-09 17:51:32 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.scss';
 import NavBar from './components/Navbar/NavBar';
 import Login from './components/Login/Login';
 import { useState } from 'react';
